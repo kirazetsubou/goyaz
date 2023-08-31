e443344a22afcbc3f9172582e152ba79cf048ed4
kirazetsubou <127604315+kirazetsubou@users.noreply.github.com>
2023-09-01 07:29:12 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function showCake() {
+    const cake = document.querySelector('.cake');
+    cake.style.display = 'block';
+    const sound = document.getElementById('sound');
+    sound.play();
+}
